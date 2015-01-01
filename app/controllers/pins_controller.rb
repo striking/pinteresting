a86ffed70aecf01dbb08c8db6615278f4c217b81
striking <chris@strikinggroup.com.au>
2015-01-01 22:04:07 +1000

    add pins/user association and authorisation

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :destroy, :edit, :update]
 
   respond_to :html
 
@@ -13,7 +15,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
     respond_with(@pin)
   end
 
@@ -21,9 +23,13 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Pin was successfully created."
+    else
+      render action: 'new'
+    end
+
   end
 
   def update
@@ -39,6 +45,11 @@ class PinsController < ApplicationController
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorised to edit this pin" if @pin.nil?
     end
 
     def pin_params
