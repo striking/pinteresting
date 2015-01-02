aa9c3e2ce62f5c2e04dd396b4aa287c8925bd58e
striking <chris@strikinggroup.com.au>
2015-01-02 19:57:37 +1000

    added minor things

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,5 +4,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }#, :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  validates :description, :image, presence: true
   
 end
